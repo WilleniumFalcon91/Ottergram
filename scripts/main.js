319d83eafc87d7e56360ef7b12439640e9870e2f
William Elliott <wcelliott1991@gmail.com>
2017-07-18 11:21:25 -0400

    creates a function that gathers img's source and title.

--- scripts/main.js
+++ scripts/main.js
@@ -3,3 +3,10 @@ var DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
 var THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
 
 
+function getImageInformation(imageUrl, titleText) {
+    var imageSrc = document.querySelector(DETAIL_IMAGE_SELECTOR);
+    imageSrc.setAttribute('src', imageUrl);
+
+    var imageTitle = document.querySelector(DETAIL_TITLE_SELECTOR);
+    imageTitle.textContent = titleText;
+}
